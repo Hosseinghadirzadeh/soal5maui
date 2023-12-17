fe452962b71b23d7e79a29070b4ea0c2f9a41562
hosseinghadirzadeh <hosseinghadirzadeh81@gmail.com>
2023-12-17 23:21:51 +0330

    done

---
+++ MAUIjsonHossein/HMjsn/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HMjsn
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUIjsonHossein/HMjsn/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace HMjsn
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<TodoItem> todoItems;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadDataAsync();
+        }
+
+        private async void LoadDataAsync()
+        {
+            string apiUrl = "https://jsonplaceholder.typicode.com/todos";
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync(apiUrl);
+            todoItems = JsonSerializer.Deserialize<ObservableCollection<TodoItem>>(json);
+            todoCollectionView.ItemsSource = todoItems;
+        }
+    }
+
+    public class TodoItem
+    {
+        [JsonPropertyName("userId")]
+        public int PostId { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("completed")]
+        public Boolean Completed { get; set; }
+    }
+}
